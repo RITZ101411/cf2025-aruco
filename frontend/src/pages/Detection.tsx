bd4f46f143587ed6b54dfa4cbc054776562bf1d8
RITZ <ritz1014.ritz1014@gmail.com>
2025-09-06 21:23:33 +0900

    add: routes

---
+++ frontend/src/pages/Detection.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Detection: React.FC = () => {
+  return (
+    <div>
+      <h1>Detection Page</h1>
+      <p>P</p>
+    </div>
+  );
+};
+
+export default Detection;
