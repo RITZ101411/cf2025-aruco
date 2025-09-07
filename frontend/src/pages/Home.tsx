90959b3e8b3f193e80f9c73e8d7ee19aed85ab6e
RITZ <ritz1014.ritz1014@gmail.com>
2025-09-07 23:22:30 +0900

    wip: aaaaaaaaaaaa

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import marker from "../assets/images/aruco_0.png"
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <p>P</p>
+      <img src="/images/aruco_0.png" alt="ArUco Marker" width={200} />
+      <img src={marker}></img>
     </div>
   );
 };
