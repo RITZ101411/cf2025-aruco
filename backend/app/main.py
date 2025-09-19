32df1ed759696bc3f3d779b2b89af18f39c693aa
RITZ <ritz1014.ritz1014@gmail.com>
2025-09-19 15:11:27 +0900

    wip

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000"],
+    allow_origins=["http://localhost:443"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
